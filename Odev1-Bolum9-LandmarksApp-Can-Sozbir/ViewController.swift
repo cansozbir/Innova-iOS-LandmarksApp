b70fe1bddb22083ddf690198f6a9f653005b59a6
Can Sozbir <cansozbirr@gmail.com>
2021-12-31 07:59:51 +0300

    Initial Commit

---
+++ Odev1-Bolum9-LandmarksApp-Can-Sozbir/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Odev1-Bolum9-LandmarksApp-Can-Sozbir
+//
+//  Created by Can Sozbir on 31.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
